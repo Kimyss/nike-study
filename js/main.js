2f5aa3c572efd63ce8c2423cf90204eaa77aa830
kimyss <kimyslive7@naver.com>
2024-03-18 16:53:43 +0900

    240318
    Always Iconic슬라이드
    메뉴바 부드럽게

---
+++ js/main.js
@@ -0,0 +1,10 @@
+new Swiper('.section-4 .swiper', {
+  loop: true, 
+  slidesPerView: 3, 
+  spaceBetween: 30, 
+  navigation: { 
+    nextEl: '.section-4 .swiper-button-next',
+    prevEl: '.section-4 .swiper-button-prev',
+  },
+});
+
