dc9be3af0efec8ae49f5e64c4ce4888e1c193d34
kimyss <kimyslive7@naver.com>
2024-03-20 16:55:20 +0900

    240320
    공부
    js js

--- js/main.js
+++ js/main.js
@@ -21,3 +21,39 @@ new Swiper('.section-5 .swiper', {
     prevEl: '.section-5 .swiper-button-prev',
   },
 });
+
+
+const fadeEls = document.querySelectorAll('.section-1 .fade-in');
+console.log(fadeEls);
+fadeEls.forEach(function (fadeEl, index) {
+  console.log(index);
+  gsap.to(fadeEl, 1, {
+    opacity: 1,
+    delay: (index + 1) * 0.3
+  });
+});
+
+const toTopEl = document.querySelector('#to-top');
+toTopEl.addEventListener('click', function () {
+  gsap.to(window, 0.6, {
+    scrollTo: 0
+  });
+});
+
+window.addEventListener('scroll', function () {
+  if (window.scrollY > 500) {
+    gsap.to(toTopEl, 1, {
+      opacity: 0.8,
+      y: 0
+    });
+  } else {
+    gsap.to(toTopEl, 1, {
+      opacity: 0,
+      y: 100,
+    });
+  }
+});
+
+const thisYear = document.querySelector('.this-year');
+thisYear.textContent = new Date().getFullYear();
+console.log(new Date().getFullYear());
